6268ab3084250c6de2e857fd203c1d36956910ad
INFORMATIQUE1\baudinthom <thomas.baudin@actudata.fr>
2023-10-18 15:21:46 +0200

    Style the signup page

--- aston_project_3/apps/account/views/signup_view.py
+++ aston_project_3/apps/account/views/signup_view.py
@@ -1,5 +1,5 @@
 """The signup view"""
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from apps.account.forms import SignupForm
 from django.views import generic
 
@@ -8,6 +8,11 @@ class SignupViewSet(generic.CreateView):
     """The signup view"""
 
     template_name = "registration/signup.html"
-    success_url = reverse_lazy("login")
     form_class = SignupForm
     success_message = "Your profile was created successfully"
+
+    def get_success_url(self) -> any:
+        return reverse_lazy(
+            "login",
+            # kwargs={"message": "Compte créé avec succès"},
+        )
