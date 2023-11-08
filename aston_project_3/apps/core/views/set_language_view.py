fa4c679748ce69f1944b7dc6cce0aea1a58f0999
INFORMATIQUE1\baudinthom <thomas.baudin@actudata.fr>
2023-11-08 09:53:44 +0100

    Secured the form a bit.

--- aston_project_3/apps/core/views/set_language_view.py
+++ aston_project_3/apps/core/views/set_language_view.py
@@ -2,17 +2,24 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import resolve, Resolver404, reverse
-from django.utils.translation import activate, get_language_from_path
+from django.utils.translation import (
+    activate,
+    check_for_language,
+    get_language_from_path,
+)
 from django.utils import translation
 from django.views import View
 from django.urls import get_resolver
 from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.urls import translate_url
 
 
 class SetLanguageViewSet(View):
     """The language view"""
 
     def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponseRedirect:
+        """
         current_url = (
             request.META.get("HTTP_REFERER")
             .replace("http://", "")
@@ -43,3 +50,43 @@ class SetLanguageViewSet(View):
             url = reverse("empty")
             response = HttpResponseRedirect(url)
             return response
+        """
+        next_url = request.POST.get("next", request.GET.get("next"))
+        if (
+            next_url or request.accepts("text/html")
+        ) and not url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            next_url = request.META.get("HTTP_REFERER")
+            if not url_has_allowed_host_and_scheme(
+                url=next_url,
+                allowed_hosts={request.get_host()},
+                require_https=request.is_secure(),
+            ):
+                next_url = "/"
+        response = (
+            HttpResponseRedirect(next_url) if next_url else HttpResponse(status=204)
+        )
+        if request.method == "POST":
+            lang_code = request.POST.get("language")
+            if lang_code and check_for_language(lang_code):
+                if next_url:
+                    url_splitted = next_url.split("/")
+                    url_splitted[1] = lang_code
+                    next_trans = "/".join(url_splitted)
+                    # next_trans = translate_url(next_url, lang_code)
+                    if next_trans != next_url:
+                        response = HttpResponseRedirect(next_trans)
+                # response.set_cookie(
+                #     settings.LANGUAGE_COOKIE_NAME,
+                #     lang_code,
+                #     max_age=settings.LANGUAGE_COOKIE_AGE,
+                #     path=settings.LANGUAGE_COOKIE_PATH,
+                #     domain=settings.LANGUAGE_COOKIE_DOMAIN,
+                #     secure=settings.LANGUAGE_COOKIE_SECURE,
+                #     httponly=settings.LANGUAGE_COOKIE_HTTPONLY,
+                #     samesite=settings.LANGUAGE_COOKIE_SAMESITE,
+                # )
+        return response
