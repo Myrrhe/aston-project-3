8283e71b76e32ff2e8c66f1cba2523c88532daeb
INFORMATIQUE1\baudinthom <thomas.baudin@actudata.fr>
2023-12-04 09:45:11 +0100

    Escaped an URI.

--- aston_project_3/static/js/forum/main.js
+++ aston_project_3/static/js/forum/main.js
@@ -3,7 +3,10 @@
 const templateURL = $("#link-section").attr("href");
 
 function getCurrentURL() {
-    return templateURL.replace("PLACEHOLDER_TYPE", $("#section-select option").val());
+    return encodeURI(templateURL.replace("PLACEHOLDER_TYPE", $("#section-select option").val()));
+    const section = $("#section-select option").val();
+    const page = $("#main").attr("data-page");
+    const category = $("#main").attr("data-category");
 }
 
 function updateURL() {
