79bfd33df594a469e9f57ee3a0b1b894bbdbdc35
unknown <thomas-baudin@sfr.fr>
2024-02-26 16:45:30 +0100

    Added mobile project.

---
+++ mobile/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "aston-project-3"
+include(":app")
