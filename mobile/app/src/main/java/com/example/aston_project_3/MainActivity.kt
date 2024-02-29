f2b50980bad9e3d12b913749f66106d3289c456e
unknown <thomas-baudin@sfr.fr>
2024-02-29 09:18:12 +0100

    Formatting.

--- mobile/app/src/main/java/com/example/aston_project_3/MainActivity.kt
+++ mobile/app/src/main/java/com/example/aston_project_3/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
