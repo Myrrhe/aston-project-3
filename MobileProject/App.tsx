797cc0a8c7c703b0737aa176bf472a5efd3e6fe5
unknown <thomas-baudin@sfr.fr>
2024-03-05 09:12:17 +0100

    Added a request.

--- MobileProject/App.tsx
+++ MobileProject/App.tsx
@@ -25,6 +25,8 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import LoginScreen from './screens/LoginScreen';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
@@ -76,6 +78,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <LoginScreen />
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
--- aston_project_3/apps/core/urls.py
+++ aston_project_3/apps/core/urls.py
@@ -1,8 +1,10 @@
 """The urls file."""
 from django.urls import path
 
+from apps.core.views import GetCSRFViewSet
 from apps.core.views import SetLanguageViewSet
 
 urlpatterns = [
     path("set-language", SetLanguageViewSet.as_view(), name="set-language"),
+    path("get-csrf", GetCSRFViewSet.as_view(), name="get-csrf"),
 ]
